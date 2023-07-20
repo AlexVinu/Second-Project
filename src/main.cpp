be6774e48a5e495e20652df795d96d3ef83791d5
Alexander <sasha65v@mail.ru>
2023-07-20 10:26:43 +0500

    two new shaders

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,6 @@ glm::vec3 cubePositions[] = {
 int WindowSizeX = 800;
 int WindowSizeY = 600;
 
-
 void WindowSizeCallback(GLFWwindow* window, int width, int height) {
     WindowSizeY = height;
     WindowSizeX = width;
@@ -57,6 +56,52 @@ void WindowSizeCallback(GLFWwindow* window, int width, int height) {
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+GLfloat points2[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+glm::vec3 LightPos(1.2f, 1.0f, 2.0f);
 
 int main(int argc, char* argv[])
 {
@@ -91,7 +136,7 @@ int main(int argc, char* argv[])
 
     std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
-    glClearColor(1, 1, 0, 0);
+    glClearColor(0, 0, 0, 0);
 
     GLuint vao = 0;
     glGenVertexArrays(1, &vao);
@@ -100,23 +145,39 @@ int main(int argc, char* argv[])
 
     Buffers::VertexBufferObject vbo;
     ResourceManager res(argv[0]);
-    auto pFirst_ShaderProgram = res.load_shader("First_ShaderProgram", "res/Shaders/vertexShader.txt", "res/Shaders/fragmentShader.txt");
     glBindVertexArray(0);
 
-    std::cerr << "1\n";
+    auto pFirst_ShaderProgram = res.load_shader("First_ShaderProgram", "res/Shaders/vertexShader.txt", "res/Shaders/fragmentShader.txt");
+    auto pLight_ShaderProgram = res.load_shader("Lamp_ShaderProgram", "res/Shaders/vertex_lampShader.txt", "res/Shaders/fragment_lampShader.txt");
+    auto pLightObject_ShaderProgram = res.load_shader("LampObject_ShaderProgram", "res/Shaders/vertex_lampShader.txt", "res/Shaders/fragment_lamp_objectShader.txt");
+
+    GLuint light_vao = 0;
+    glGenVertexArrays(1, &light_vao);
+    glBindVertexArray(light_vao);
+
+    GLuint light_vbo = 0;
+    glGenBuffers(1, &light_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, light_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points2), points2, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+
+    glBindVertexArray(0);
+
+    std::cout << "1\n";
 
     auto brick = res.load_texture("brick", "res/textures/brick.png");
 
-    std::cerr << "2\n";
+    std::cout << "2\n";
 
     auto smile = res.load_texture("smile", "res/textures/smile.png");
 
     auto wood = res.load_texture("wood", "res/textures/wood.png");
 
-    std::cerr << "end\n";
+    std::cout << "end\n";
     res.getObjects_Textures();
     res.getObjects_Shaders();
-
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -125,20 +186,13 @@ int main(int argc, char* argv[])
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-        pFirst_ShaderProgram->use_ShaderProgram();
-
-        
         glm::mat4 projection(1.0f);
-
-       
-
         glm::mat4 view;
         view = camera.viewMatrix();
 
-
         projection = glm::perspective(camera.fov, (GLfloat)WindowSizeX/ (GLfloat)WindowSizeY, 0.1f, 100.0f);
 
+        pFirst_ShaderProgram->use_ShaderProgram(); //First shader
 
         GLint modelLoc = glGetUniformLocation(pFirst_ShaderProgram->Give_Id(), "model");
 
@@ -148,9 +202,55 @@ int main(int argc, char* argv[])
         GLint projectionLoc = glGetUniformLocation(pFirst_ShaderProgram->Give_Id(), "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        glBindVertexArray(vao);
+        pLight_ShaderProgram->use_ShaderProgram(); //Second shader
+
+        GLint modelLoc2 = glGetUniformLocation(pLight_ShaderProgram->Give_Id(), "model");
+
+        GLint viewLoc2 = glGetUniformLocation(pLight_ShaderProgram->Give_Id(), "view");
+        glUniformMatrix4fv(viewLoc2, 1, GL_FALSE, glm::value_ptr(view));
+
+        GLint projectionLoc2 = glGetUniformLocation(pLight_ShaderProgram->Give_Id(), "projection");
+        glUniformMatrix4fv(projectionLoc2, 1, GL_FALSE, glm::value_ptr(projection));
+
+        GLint lightColor_loc = glGetUniformLocation(pLight_ShaderProgram->Give_Id(), "lightColor");
+        GLint objectColor_loc = glGetUniformLocation(pLight_ShaderProgram->Give_Id(), "objectColor");
+
+        glUniform3f(lightColor_loc, 1.0f, 1.0f, 1.0f);
+        glUniform3f(objectColor_loc, 1.0f, 0.5f, 0.31f);
+
+        glm::mat4 model2(1.0f);
+        model2 = glm::scale(model2, glm::vec3(3.0f));
+
+        glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(model2));
+
+        glBindVertexArray(light_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        pLightObject_ShaderProgram->use_ShaderProgram(); //third shader
+
+        GLint viewLoc3 = glGetUniformLocation(pLightObject_ShaderProgram->Give_Id(), "view");
+        glUniformMatrix4fv(viewLoc3, 1, GL_FALSE, glm::value_ptr(view));
+
+        GLint projectionLoc3 = glGetUniformLocation(pLightObject_ShaderProgram->Give_Id(), "projection");
+        glUniformMatrix4fv(projectionLoc3, 1, GL_FALSE, glm::value_ptr(projection));
+
+        GLint modelLoc3 = glGetUniformLocation(pLightObject_ShaderProgram->Give_Id(), "model");
+
+        glm::mat4 model3(1.0f);
+        model3 = glm::translate(model3, glm::vec3(5.0f, 0.0f, -5.0f));
+        glUniformMatrix4fv(modelLoc3, 1, GL_FALSE, glm::value_ptr(model3));
+
+        glBindVertexArray(light_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
         for (int i = 0; i < 10; i++) {
             if (i < 6) {
+                pFirst_ShaderProgram->use_ShaderProgram();
+
+                glBindVertexArray(vao);
+
                 glActiveTexture(GL_TEXTURE0);
                 glBindTexture(GL_TEXTURE_2D, wood->Give_ID());
                 glUniform1i(glGetUniformLocation(pFirst_ShaderProgram->Give_Id(), "ourTexture1"), 0);
@@ -163,8 +263,13 @@ int main(int argc, char* argv[])
                 GLfloat angle = i - 2.0f;
                 model = glm::rotate(model, (GLfloat)glfwGetTime() * angle, glm::vec3(1.0f, 0.0f, 1.0f));
                 glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+                glBindVertexArray(0);
             }
             else {
+                glBindVertexArray(vao);
+
                 glActiveTexture(GL_TEXTURE0);
                 glBindTexture(GL_TEXTURE_2D, brick->Give_ID());
                 glUniform1i(glGetUniformLocation(pFirst_ShaderProgram->Give_Id(), "ourTexture1"), 0);
@@ -177,10 +282,11 @@ int main(int argc, char* argv[])
                 GLfloat angle = i - 2.0f;
                 model = glm::rotate(model, (GLfloat)glfwGetTime() * angle, glm::vec3(1.0f, 0.0f, 1.0f));
                 glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+                glBindVertexArray(0);
             }
-            glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-        glBindVertexArray(0);
         
         GLfloat currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
@@ -194,6 +300,7 @@ int main(int argc, char* argv[])
         camera.keyboard_move(deltaTime, keys);
     }
     glDeleteVertexArrays(1, &vao);
+    glDeleteVertexArrays(1, &light_vao);
     glfwTerminate();
     return 0;
 }
